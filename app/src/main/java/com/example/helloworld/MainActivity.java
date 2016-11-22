0ad4fa7ecfcdfdf589725e1f089f3b274d4eb050
GreatDragon.Kim <multikty@yahoo.com>
2016-11-22 16:31:12 +0900

    Hello World! First Modified.

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView main_TextView = (TextView) findViewById(R.id.main_TextView);
+        main_TextView.setText("Hello World!" + "\n" +
+                                "First Modified");
+    }
+}
